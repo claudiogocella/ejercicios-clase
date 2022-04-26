504f4d7f6726703ff3874d894ce5431eca38670b
claudiogocella <104107475+claudiogocella@users.noreply.github.com>
2022-04-26 14:38:09 -0300

    Initial commit

---
+++ la-granjera.ts
@@ -0,0 +1,7 @@
+console.log("mientras que haya camino hacia adelante");
+console.log("avanzar");
+console.log("cavar");
+console.log("girar a la izquierda");
+console.log("mientras que haya camino hacia adelante");
+console.log("avanzar");
+console.log("cavar");
